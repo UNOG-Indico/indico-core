61157f71242426b7af8aa0fb0c3c315f6c1fff06
Segilola Mustapha <segilola.mustapha@un.org>
2025-09-29 20:20:56 +0200

    Fix downgrade script

--- indico/migrations/versions/20250821_1346_6059028b3e64_add_regform_templates_at_category.py
+++ indico/migrations/versions/20250821_1346_6059028b3e64_add_regform_templates_at_category.py
@@ -50,12 +50,39 @@ def upgrade():
 
 
 def downgrade():
+    conn = op.get_bind()
+    form_ids = [form for form, in conn.execute(
+        sa.text('SELECT id FROM event_registration.forms WHERE category_id IS NOT NULL')
+    )]
+    if form_ids:
+        form_item_ids = [item for item, in conn.execute(
+            sa.text('SELECT id FROM event_registration.form_items WHERE registration_form_id = ANY(:form_ids)'),
+            {'form_ids': form_ids}
+        )]
+        if form_item_ids:
+            conn.execute(
+                sa.text('UPDATE event_registration.form_items SET current_data_id = NULL WHERE id = ANY(:form_item_ids)'),
+                {'form_item_ids': form_item_ids}
+            )
+            conn.execute(
+                sa.text('DELETE FROM event_registration.form_field_data WHERE field_id = ANY(:item_ids)'),
+                {'item_ids': form_item_ids}
+            )
+            conn.execute(
+                sa.text('DELETE FROM event_registration.form_items WHERE id = ANY(:item_ids)'),
+                {'item_ids': form_item_ids}
+            )
+        conn.execute(
+            sa.text('DELETE FROM event_registration.forms WHERE id = ANY(:form_ids)'),
+            {'form_ids': form_ids}
+        )
     op.execute('DROP TRIGGER IF EXISTS registrations_prevent_forms_linked_to_categories ON event_registration.registrations;')
     op.execute('DROP FUNCTION IF EXISTS disallow_registration_on_forms_linked_to_categories;')
-    op.execute('''DELETE FROM event_registration.forms WHERE category_id IS NOT NULL; ''')  # I thought there was cascading delete between registration form and the form fields?
+    op.execute('''DELETE FROM event_registration.forms WHERE category_id IS NOT NULL; ''')
     op.drop_constraint('ck_forms_event_xor_category_id_null', 'forms', schema='event_registration')
     op.drop_column('forms', 'category_id', schema='event_registration')
     op.drop_column('forms', 'cloned_from_id', schema='event_registration')
     op.alter_column('forms', 'event_id', existing_type=sa.Integer(), nullable=False, schema='event_registration')
+    op.execute('''DELETE FROM categories.logs WHERE realm = 3;''')
     op.drop_constraint('ck_logs_valid_enum_realm', 'logs', schema='categories')
     op.create_check_constraint('valid_enum_realm', 'logs', '(realm = ANY (ARRAY[1, 2]))', schema='categories')
