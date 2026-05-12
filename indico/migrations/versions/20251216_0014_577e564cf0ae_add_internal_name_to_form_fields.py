d6e6ff8d6156084eda19e209b98375470633e265
Tamas Mako <1412056+tomako@users.noreply.github.com>
2026-05-12 13:26:47 +0200

    PATCH Disable CK in field internal name migration

--- indico/migrations/versions/20251216_0014_577e564cf0ae_add_internal_name_to_form_fields.py
+++ indico/migrations/versions/20251216_0014_577e564cf0ae_add_internal_name_to_form_fields.py
@@ -5,7 +5,7 @@ Revises: af9d03d7073c
 Create Date: 2025-12-16 00:14:49.123118
 """
 
-from enum import Enum
+# from enum import Enum
 
 import sqlalchemy as sa
 from alembic import op
@@ -18,19 +18,22 @@ branch_labels = None
 depends_on = None
 
 
-class _PersonalDataType(Enum):
-    email = 1
-    first_name = 2
-    last_name = 3
-    affiliation = 4
-    title = 5
-    address = 6
-    phone = 7
-    country = 8
-    position = 9
-    picture = 10
+# class _PersonalDataType(Enum):
+#     email = 1
+#     first_name = 2
+#     last_name = 3
+#     affiliation = 4
+#     title = 5
+#     address = 6
+#     phone = 7
+#     country = 8
+#     position = 9
+#     picture = 10
 
-
+# HACK: Part of this migration script is disabled because personal data types are
+#       patched in the plugin. Running it like this will always fail. Instead,
+#       we run the script from the plugin adding the entire list of valid values.
+#       See un/migrations/20260511_1603_a43c305d9c7a_update_internal_name_of_pd_form_fields.py
 def upgrade():
     op.add_column('form_items', sa.Column('internal_name', sa.String(), index=True, nullable=True),
                   schema='event_registration')
@@ -40,18 +43,18 @@ def upgrade():
         "internal_name != ''",
         schema='event_registration'
     )
-    case_expr = ' '.join(f"WHEN {e.value} THEN '{e.name.replace('_', '-')}'" for e in _PersonalDataType)
-    op.execute(f'''
-        UPDATE event_registration.form_items
-        SET internal_name = CASE personal_data_type {case_expr} END
-        WHERE personal_data_type IS NOT NULL;
-    ''')  # noqa: S608
-    op.create_check_constraint(
-        'pd_internal_name_required',
-        'form_items',
-        '(internal_name IS NOT NULL) OR (personal_data_type IS NULL)',
-        schema='event_registration'
-    )
+    # case_expr = ' '.join(f"WHEN {e.value} THEN '{e.name.replace('_', '-')}'" for e in _PersonalDataType)
+    # op.execute(f'''
+    #     UPDATE event_registration.form_items
+    #     SET internal_name = CASE personal_data_type {case_expr} END
+    #     WHERE personal_data_type IS NOT NULL;
+    # ''')
+    # op.create_check_constraint(
+    #     'pd_internal_name_required',
+    #     'form_items',
+    #     '(internal_name IS NOT NULL) OR (personal_data_type IS NULL)',
+    #     schema='event_registration'
+    # )
 
 
 def downgrade():
